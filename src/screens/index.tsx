6ecd8b5f7258f545f55ce101eb75f5444231fcf2
sumit7577 <asmitkumarr082@gmail.com>
2024-05-03 14:37:25 -0700

    some fix

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -10,31 +10,39 @@ import { DropDown } from "../components/DropDown";
 import { useState } from "react";
 import Button from "../components/Button";
 import Modal from "../components/Modal";
+import BBC from "../assets/icons/bbc.png";
+import BNC from "../assets/icons/bnc.png";
+import BTC from "../assets/icons/btc.png";
+import DOLLAR from "../assets/icons/dollar.png";
+import ETHERUM from "../assets/icons/eth.png";
+import GRAPH from "../assets/icons/graph.png";
+import TCOIN from "../assets/icons/tcoin.png";
 
 
 const Stepper = () => {
     return (
         <ol className="relative text-gray-500 border-s border-gray-500 dark:border-gray-700 dark:text-gray-400">
+
             <li className="mb-10 ms-6">
-                <span className="absolute flex items-center justify-center w-[33px] h-[33px] rounded-full -start-4 ring-1 ring-slate-600">
+                <span className="absolute flex items-center justify-center w-[33px] h-[33px] bg-primary rounded-full -start-4 ring-1 ring-slate-600">
                     <h2>1</h2>
                 </span>
                 <h3 className="font-[600] text-[20px] text-black">Starting Bridge Transaction</h3>
             </li>
             <li className="mb-10 ms-6">
-                <span className="absolute flex items-center justify-center w-[33px] h-[33px] rounded-full -start-4 ring-1 ring-slate-600">
+                <span className="absolute flex items-center justify-center w-[33px] h-[33px] bg-primary rounded-full -start-4 ring-1 ring-slate-600">
                     <h2>2</h2>
                 </span>
                 <h3 className="font-[600] text-[20px] text-black">Crossing Bridge</h3>
             </li>
             <li className="mb-10 ms-6">
-                <span className="absolute flex items-center justify-center w-[33px] h-[33px] rounded-full -start-4 ring-1 ring-slate-600">
+                <span className="absolute flex items-center justify-center w-[33px] h-[33px] bg-primary rounded-full -start-4 ring-1 ring-slate-600">
                     <h2>3</h2>
                 </span>
                 <h3 className="font-[600] text-[20px] text-black">Approving Transfer</h3>
             </li>
             <li className="ms-6">
-                <span className="absolute flex items-center justify-center w-[33px] h-[33px] rounded-full -start-4 ring-1 ring-slate-600">
+                <span className="absolute flex items-center justify-center w-[33px] h-[33px] bg-primary rounded-full -start-4 ring-1 ring-slate-600">
                     <h2>4</h2>
                 </span>
                 <h3 className="font-[600] text-[20px] text-black">Complete</h3>
@@ -68,7 +76,37 @@ const Home = () => {
     const [toNetwork, setToNetwork] = useState<typeof coins[0]>(coins[1]);
     const [modal, setModal] = useState<boolean>(false);
     return (
-        <Grid className={`bg-gradient-to-r from-primaryho to-primary w-[100vw] h-[100vh] justify-center`}>
+        <Grid className={`bg-gradient-to-b from-[#F2C2D3] to-[#A47989] w-[100vw] h-[100vh] justify-center`}>
+            <Block className="absolute left-[4%] top-[1%] bg-[rgba(131,189,103,0.2)] rounded-full w-[186px] h-[186px] justify-center">
+                <Image src={DOLLAR} classname="w-[55px] h-[88px]" />
+            </Block>
+
+
+            <Block className="absolute left-[49%] top-[2%] bg-[rgba(247,147,26,0.2)] rounded-full w-[105px] h-[105px] justify-center">
+                <Image src={BTC} classname="w-[34px] h-[45px]" />
+            </Block>
+
+            <Block className="absolute left-[6%] top-[63%] bg-[rgba(255,255,255,0.2)] rounded-full w-[168px] h-[168px] justify-center">
+                <Image src={ETHERUM} classname="w-[47px] h-[80px]" />
+            </Block>
+
+            <Block className="absolute left-[79%] top-[47%] bg-[rgb(243,186,47,0.2)] rounded-full w-[171px] h-[171px] justify-center">
+                <Image src={BNC} classname="w-[92px] h-[92px]" />
+            </Block>
+
+            <Block className="absolute left-[66%] top-[60%] bg-[rgba(60,200,200,0.2)] rounded-full w-[122px] h-[122px] justify-center">
+                <Image src={GRAPH} classname="w-[70px] h-[66px]" />
+            </Block>
+
+            <Block className="absolute left-[43%] top-[90%] bg-[rgba(0,164,120,0.2)] rounded-full w-[110px] h-[110px] justify-center">
+                <Image src={TCOIN} classname="w-[47px] h-[42px]" />
+            </Block>
+
+            <Block className="absolute left-[88%] top-[85%] bg-[rgba(240,64,134,0.2)] rounded-full w-[76px] h-[76px] justify-center">
+                <Image src={BBC} classname="!w-[38px] !h-[35px]" />
+
+            </Block>
+
             <Block className="absolute right-[100px] top-[122px]">
                 <Stepper />
             </Block>
@@ -138,7 +176,7 @@ const Home = () => {
 
             <Modal title={"Connect Wallet"}
                 open={modal} setModal={setModal}
-                titleclassName="!text-[24px] text-center !font-[600]"
+                titleClass="!text-[24px] text-center !font-[600]"
                 classname="w-[507px] h-[431px]"
                 onSuccess={() => {
                     setModal(() => !modal)
